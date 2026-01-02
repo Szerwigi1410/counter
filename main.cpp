0e01d0b8acd1fe7c6a3f4d5e42b6637682c07a16
Szerwigi1410 <szerwigi14@gmail.com>
2026-01-02 23:59:01 +0100

    tried to add a timer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
-start:    
+start:
+    //auto x = high_resolution_clock::now();
     long long a = 0;
     long long b = 0;
     int choice;
@@ -16,10 +19,12 @@ start:
         cin >> a;
         cout << "To: ";
         cin >> b;
-
+        auto x = high_resolution_clock::now();
         for (int i = a; i <= b; i++) {
             cout << i << endl;
         }
+        auto y = high_resolution_clock::now();
+        cout << "took " << duration_cast<seconds>(y - x).count() << " seconds" <<  endl;
         break;
     
     case 2:
@@ -27,10 +32,12 @@ start:
         cin >> a;
         cout << "To: ";
         cin >> b;
-
+        auto x = high_resolution_clock::now();
         for (int i = a; i <= b; i++) {
             cout << "\r" << i << flush;
         }
+        auto y = high_resolution_clock::now();
+        cout << "took " << duration_cast<seconds>(y - x).count() << " seconds" <<  endl;
         break;
     default:
         cout << "not an option!" << endl;
